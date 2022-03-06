2f96449f5f4627a81afde890674809256306d2e4
Kirsteenng <kirsteenng94@gmail.com>
2022-03-06 15:56:40 -0800

    Update random_select.py

--- random_select.py
+++ random_select.py
@@ -12,8 +12,8 @@ import json
 import random
 import re
 
-df = np.load('/Users/Kirsteenng_1/Downloads/share/longformer_all_review_predicted_score.npy')
-test_path = '/Users/Kirsteenng_1/Downloads/share/test_reviews.jsonl'
+df = np.load('./longformer_all_review_predicted_score.npy')
+test_path = './test_reviews.jsonl'
 
 
 def parse_and_select(data:pd.DataFrame)->list:
@@ -42,25 +42,8 @@ data = pd.DataFrame(json_list)
 data['random_sentence'] = parse_and_select(data)
 data = data.drop(['reviews', 'scores','avg_score'],axis = 1)
 
-data.to_csv('/Users/Kirsteenng_1/Downloads/share/test_reviews_with_random.csv')
+data.to_csv('./test_reviews_with_random.csv')
 
-
-random_select = []
-for i in range(0,len(data)):
-    random_sentence = []
-    full_review = ' '.join(data['reviews'][i])
-    temp_sentence = re.sub('\n','',full_review)
-    temp_sentence = re.sub(r'[.?!]+\s+', '<end>', temp_sentence)
-    sentence_list = temp_sentence.split('<end>')
-    random_sentence += random.sample(sentence_list,round(len(sentence_list)*0.3))
-    print(len(random_sentence))
-    
-    random_select.append(random_sentence)
-    print(len(random_select))
-    
-    #print(random_select)
-    
-data['random_sentence'] = random_select
     
     
     
